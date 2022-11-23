e3afe5dc32acd7a86c039704fc0a80099e8b2148
Vittorio <vittfassano@gmail.com>
2022-11-23 16:29:15 -0300

    reafact: observacoes

--- Observacoes.js
+++ Observacoes.js
@@ -1,6 +1,9 @@
 /*
 
-01) *rota signin: https://brechofut.onrender.com
+01)link do deploy do back:
+        ***  https://brechofut.onrender.com   ***
+
+    *rota signin: https://brechofut.onrender.com
     *rota signup: https://brechofut.onrender.com/signup
 
 02)Sign-up e Sign-in feitos e funcionando;
@@ -8,6 +11,9 @@
 03)deploy do bando de dados no atlas feito e funcionando;
 
 04)deploy do back no render feito e funcionando;
+
+05)link do deploy do banco de dados:
+MONGO_URI = mongodb+srv://root:root@brechofut-cluster.nyqcn95.mongodb.net/?retryWrites=true&w=majority
 
 */
 
