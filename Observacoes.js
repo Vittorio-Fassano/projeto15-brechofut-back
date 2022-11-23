f6d50a4b743690a4af202d56e5654c149e2d60d8
Vittorio <vittfassano@gmail.com>
2022-11-23 14:00:35 -0300

    feat: signin & signup

---
+++ Observacoes.js
@@ -0,0 +1,13 @@
+/*
+
+01) *rota signin: https://brechofut.onrender.com
+    *rota signup: https://brechofut.onrender.com/signup
+
+02)Sign-up e Sign-in feitos e funcionando;
+
+03)deploy do bando de dados no atlas feito e funcionando;
+
+04)deploy do back no render feito e funcionando;
+
+*/
+
