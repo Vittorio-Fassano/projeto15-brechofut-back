ac6c08180a5a15051badb59dc7b3cdde441102f9
Vittorio <vittfassano@gmail.com>
2022-11-23 11:50:00 -0300

    feat: prepare to deploy backend 02

--- src/database/db.js
+++ src/database/db.js
@@ -13,7 +13,7 @@ try {
     console.log(chalk.bold.red("Error connecting with mongoDb", err));
 }
 
-const database = process.env.MONGO_DB;
+const database = "brechofut-database";
 let db = null;
 
 try {
