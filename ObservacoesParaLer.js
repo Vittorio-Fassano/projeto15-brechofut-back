842e832aee06f465fa0b5a04ea7dc6c67ffbdefa
Vittorio <vittfassano@gmail.com>
2022-11-23 11:37:17 -0300

    feat: deploy database

--- ObservacoesParaLer.js
+++ ObservacoesParaLer.js
@@ -3,7 +3,11 @@
 01) por enquanto deixei o arquivo .env fora do git ignore para que você consiga pegar a URI do
 mongo que estou usando e testar os dados no thunderclient;
 
-02);
+02)Sign-up e Sign-in feitos e funcionando;
+
+03)deploy do bando de dados no atlas feito e funcionando;
+
+04)
 
 
 */
