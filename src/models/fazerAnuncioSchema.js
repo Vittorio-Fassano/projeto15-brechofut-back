a3b9ca6984d7e1922e5b670c462c4d0ceb9b76b0
Vittorio <vittfassano@gmail.com>
2022-11-26 11:36:09 -0300

    refact: novoAnuncio2

--- src/models/fazerAnuncioSchema.js
+++ src/models/fazerAnuncioSchema.js
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export const novoAnuncioSchema = Joi.object ({
     value: Joi.number().positive().required(),
-    description: Joi.string().min(15).max(50).required(),
+    description: Joi.string().min(20).max(50).required(),
     image: Joi.string().uri().min(10).required()
 })
